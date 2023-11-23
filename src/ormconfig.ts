635e5b7cedc0a79549aba917be9ca80da8b3bdd2
lizezhi <1>
2023-11-23 15:41:38 +0800

    init

---
+++ src/ormconfig.ts
@@ -0,0 +1,29 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const VercelConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'ep-aged-waterfall-13013279-pooler.us-east-1.postgres.vercel-storage.com',
+  port: 5432,
+  username: 'default',
+  password: 'WH6FZNOe5asP',
+  database: 'verceldb',
+  ssl: { rejectUnauthorized: false }, // For local development, consider removing this in production
+  synchronize: true, // Automatically create database tables based on entities (set to false in production)
+  logging: true, // Enable SQL logging
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+};
+
+const MySqlLocalConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '123456',
+  database: 'nesttest',
+  synchronize: true,
+  retryDelay: 500,
+  retryAttempts: 10,
+  autoLoadEntities: true,
+};
+
+export { VercelConfig, MySqlLocalConfig };
---
+++ src/swagger.ts
@@ -0,0 +1,20 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageConfig from '../package.json';
+import { INestApplication } from '@nestjs/common';
+
+export const generateDocument = (app: INestApplication<any>) => {
+  const swaggerCDN = 'https://js.cdn.haah.net/npm/swagger-ui-dist@5.7.2';
+  const options = new DocumentBuilder()
+    .setTitle(packageConfig.name)
+    .setDescription(packageConfig.description)
+    .setVersion(packageConfig.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/swagger', app, document, {
+    customCssUrl: [`${swaggerCDN}/swagger-ui.css`],
+    customJs: [
+      `${swaggerCDN}/swagger-ui-bundle.js`,
+      `${swaggerCDN}/swagger-ui-standalone-preset.js`,
+    ],
+  });
+};
