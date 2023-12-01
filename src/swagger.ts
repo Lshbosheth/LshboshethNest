83c6f00d9a462b6149b0ba1b56145793653f4e01
lizezhi <1>
2023-12-01 13:42:49 +0800

    fix

--- src/swagger.ts
+++ src/swagger.ts
@@ -17,6 +17,6 @@ export const generateDocument = (app: INestApplication<any>) => {
       `${swaggerCDN}/swagger-ui-standalone-preset.js`,
     ],
     customSiteTitle: 'Lshbosheth | API',
-    customfavIcon: 'favicon.ico',
+    customfavIcon: '/favicon.ico',
   });
 };
