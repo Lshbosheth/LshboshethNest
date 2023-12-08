7f8f788ce3c1e6126f030055c5710a57ec805eb0
lizezhi <1>
2023-12-08 10:58:45 +0800

    fix

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import { areas } from '../static/areas';
 import { QrCodeDto } from './dto/qrCode.dto';
 import { CreateConfigDto } from './dto/create-config.dto';
 import { UpdateConfigDto } from './dto/update-config.dto';
 import * as QRCode from 'qrcode';
-import { del, list, ListFoldedBlobResult } from '@vercel/blob';
+import { del, list } from '@vercel/blob';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Config } from './entities/config.entity';
