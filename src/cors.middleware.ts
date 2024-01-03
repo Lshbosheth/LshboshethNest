a888db1f2e3c872d91bbf7d3bdab5af720afa9ec
lizezhi <1>
2024-01-03 10:11:03 +0800

    fix

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -1,13 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import * as cors from 'cors';
+import * as express from 'express';
+import cors from 'cors';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: any) {
-    const options: cors.CorsOptions = {
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      origin: '*',
-    };
-    return cors(options)(req, res, next);
+  private readonly corsMiddleware = cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: '*',
+  });
+
+  use(req: Request | any, res: Response | any, next: any) {
+    const request = req as express.Request;
+    const response = res as express.Response;
+    this.corsMiddleware(request, response, next);
   }
 }
